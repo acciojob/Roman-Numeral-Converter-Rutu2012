07bcb35cefe3252b73a4f5fed381c78b7fc1813a
Rutu2012 <sanghanirutu944@gmail.com>
2025-05-12 13:47:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,31 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000],
-	  1:['CM',900],
-      2:['D', 500],
-	  3:['CD',400],
-      4:['C', 100],
-	  5:['XC',90],
-      6:['L', 50], 
-      7:['XL',49], 
-      8:['X',10], 
-      9:['IX',9],
-	  10:['V',5],
-	  11:['IV',4],
-	  12:['I',1]
-    };
-	
-let num=798;
-let ans=""
-	while(num==!0){
-		for(let i in obj)
-		{
-		    let[symbol,value]=obj[i];
-			if(value<=num)
-			{
-				ans=ans+symbol;
-				num=num-value;
-				break;
-			}
-		}
-	}
-  
-return ans;
+  const romanMap = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
+
+  let result = "";
+
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+console.log(convertToRoman(36)); // Output: XXXVI
+module.exports = convertToRoman;
